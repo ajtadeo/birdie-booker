b61d506e96a7ce4869ec834ed6159a10cfaf3f2b
Alyssa Tadeo <alyssajtadeo@gmail.com>
2023-09-10 10:35:41 +0100

    flask app displaying alerts

--- web/app/__init__.py
+++ web/app/__init__.py
@@ -1,7 +1,54 @@
 from flask import Flask, render_template
+import sqlite3
+import os
 
+def getDB():
+	path = os.path.dirname(__file__)
+	conn = sqlite3.connect(os.path.join(path, "alerts.db"))
+	cursor = conn.cursor()
+	return conn, cursor
+
+def save(location, numPlayers, date, startTime, endTime):
+	print(f"Saving Alert: {location}, {numPlayers}, {date}, {startTime}, {endTime}")
+	sql = """
+	INSERT INTO `alerts` (`location`, `numPlayers`, `date`, `startTime`, `endTime`)
+	VALUES (?, ?, ?, ?, ?)
+	"""
+	conn, cursor = getDB()
+	cursor.execute(sql, (location, numPlayers, date, startTime, endTime))
+	conn.commit()
+	conn.close()
+ 
+# intialize app
 app = Flask(__name__)
 
+# intialize database
+path = os.path.dirname(__file__)
+conn, cursor = getDB()
+sql = """
+CREATE TABLE IF NOT EXISTS `alerts` (
+	`id` INTEGER PRIMARY KEY,
+	`location` TEXT,
+	`numPlayers` INTEGER,
+	`date` TEXT,
+	`startTime` TEXT,
+	`endTime` TEXT,
+	`isExpired` INTEGER DEFAULT 0
+)
+"""
+cursor.execute(sql)
+conn.commit()
+conn.close()
+
+# routes
 @app.route('/')
 def index():
 	return render_template("index.html")
+
+@app.route('/birdie-booker')
+def birdieBooker():
+	conn, cursor = getDB()
+	alerts = conn.execute("SELECT * FROM `alerts`").fetchall()
+	print(alerts)
+	conn.close()
+	return render_template("birdieBooker.html", data=alerts)
--- web/app/db.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-import os
-
-# setup database connection
-path = os.path.dirname(__file__)
-CONN = sqlite3.connect(os.path.join(path, "alerts.db"))
-CURSOR = CONN.cursor()
