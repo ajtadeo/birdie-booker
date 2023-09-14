13cd8e72bc18701e60b380fd753d594233f4d385
Alyssa Tadeo <alyssajtadeo@gmail.com>
2023-09-14 14:10:14 -0700

    adding alerts to db via form

--- web/app/birdie_booker/views.py
+++ web/app/birdie_booker/views.py
@@ -1,9 +1,10 @@
 import os
 import sqlite3
-from flask import Blueprint, render_template, redirect
+from flask import Blueprint, render_template, redirect, url_for
 from datetime import datetime, timedelta
 from flask_wtf import FlaskForm
-from wtforms import Form, DateField, SelectField, TimeField, validators
+from wtforms import Form, DateField, SelectField, TimeField
+from wtforms.validators import InputRequired, ValidationError
 
 LOCATIONS = [
 	(0, 'Recreation Park 18 Golf Course')
@@ -17,23 +18,51 @@ def getDB():
 	cursor = conn.cursor()
 	return conn, cursor
 
-def save(location, numPlayers, date, startTime, endTime):
-	print(f"Saving Alert: {location}, {numPlayers}, {date}, {startTime}, {endTime}")
+def save(location, numPlayers, date, startTime, endTime, isExpired):
+	print(f"Saving Alert: {location}, {numPlayers}, {date}, {startTime}, {endTime}, {isExpired}")
 	sql = """
-	INSERT INTO `alerts` (`location`, `numPlayers`, `date`, `startTime`, `endTime`)
-	VALUES (?, ?, ?, ?, ?)
+	INSERT INTO `alerts` (`location`, `numPlayers`, `date`, `startTime`, `endTime`, `isExpired`)
+	VALUES (?, ?, ?, ?, ?, ?)
 	"""
-	conn, cursor = getDB()
-	cursor.execute(sql, (location, numPlayers, date, startTime, endTime))
-	conn.commit()
-	conn.close()
+	try:
+		conn, cursor = getDB()
+		cursor.execute(sql, (location, numPlayers, date, startTime, endTime, isExpired))
+		conn.commit()
+		conn.close()
+	except Exception as err:
+		print("Saving to DB failed.")
+		print(err)
+ 
+def validate_date(form, date):
+	if date.data < datetime.today().date():
+		raise ValidationError("Date must be today or a future date.")
+
+def validate_endTime(form, endTime):
+	if endTime.data <= form.startTime.data:
+		raise ValidationError("End Time must be after start time.")
  
 class AlertForm(FlaskForm):
-	location = SelectField('Location', choices=LOCATIONS)
-	numPlayers = SelectField('Number of Players', choices=[(1, 1), (2, 2), (3, 3), (4, 4)])
-	date = DateField('Date', format='%m/%d/%Y')
-	startTime = TimeField('Start Time')
-	endTime = TimeField('End Time')
+	location = SelectField('Location', [
+		InputRequired()
+	], choices=LOCATIONS)
+
+	numPlayers = SelectField('Number of Players', [
+		InputRequired()
+	], choices=[(1, 1), (2, 2), (3, 3), (4, 4)])
+
+	date = DateField('Date', [
+		InputRequired(),
+		validate_date
+	])
+
+	startTime = TimeField('Start Time', [
+		InputRequired()
+  ])
+
+	endTime = TimeField('End Time', [
+		InputRequired(),
+		validate_endTime
+  ])
 
 
 @birdie_booker.route("/", methods=['GET', 'POST'])
@@ -41,20 +70,20 @@ def birdie_booker_view():
 	# get alerts
 	conn, cursor = getDB()
 	alerts = conn.execute("SELECT * FROM `alerts`").fetchall()
-	print(alerts)
 	conn.close()
  
 	# handle form submission
 	form = AlertForm()
-	if form.is_submitted():
+	if form.validate_on_submit():
 		location = form.location.data
 		numPlayers = form.numPlayers.data
-		date = form.date.data.strftime("%m/%d/%Y")
-		startTime = form.startTime.data.strftime("%H:%M")
-		endTime = form.endTime.data.strftime("%H:%M")
+		date = form.date.data.strftime("%a %m/%d/%Y")
+		startTime = form.startTime.data.strftime("%I:%M %p")
+		endTime = form.endTime.data.strftime("%I:%M %p")
+		isExpired = 0  # form validation requires date to be un-expired
 
-		save(location=location, numPlayers=numPlayers, date=date, startTime=startTime, endTime=endTime)
-		return redirect("/")
+		save(location=location, numPlayers=numPlayers, date=date, startTime=startTime, endTime=endTime, isExpired=isExpired)
+		return redirect(url_for("birdie-booker.birdie_booker_view"))
 
 	# TODO: alerts not showing up , possibly because of None value in the database. need to crate validation functions
-	return render_template("birdie_booker.html", alerts=alerts, form=form)
+	return render_template("birdie_booker.html", data=alerts, form=form)
